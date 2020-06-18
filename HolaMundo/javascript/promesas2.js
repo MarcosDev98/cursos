fb110fcb75917ed56a0b36a86a335c72e0e6ab71
dado1314 <35411452+dado1314@users.noreply.github.com>
2020-06-18 00:45:10 -0300

    Desde la notebook: metí todos los archivos js en una carpeta y creé 3 txt con ejercicios reales.

---
+++ HolaMundo/javascript/promesas2.js
@@ -0,0 +1,17 @@
+/* require('isomorphic-fetch')
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(x => x.json())
+    .then(x => console.log(x))
+
+ */
+
+
+//Así seria sin promesas
+ /* $.get('https://jsonplaceholder.typicode.com/users', (res) =>{
+     $.get('https://jsonplaceholder.typicode.com/users')
+ }) 
+ */
+
+
+
