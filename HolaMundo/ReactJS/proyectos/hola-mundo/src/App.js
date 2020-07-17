bba1e6331e488a3a66ce775e44b1d6082c713678
MarcosDev98 <marcosolda@outlook.es>
2020-07-17 16:27:55 -0300

    Desde PC: Cree una capeta para practicar HTML y avance en el curso de ReactJS desde 0 de HolaMundo

--- HolaMundo/ReactJS/proyectos/hola-mundo/src/App.js
+++ HolaMundo/ReactJS/proyectos/hola-mundo/src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Cabecera from "./components/Cabecera";
+import "./App.css";
 
 function App() {
+  function manejaClick(texto) {
+    console.log(texto);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cabecera miau={"Bienvenido miau"} manejaClick={manejaClick} />
     </div>
   );
 }
