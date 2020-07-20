ab4032fb86cb7c371407776fe88299b61d6c735e
MarcosDev98 <marcosolda@outlook.es>
2020-07-19 22:39:40 -0300

    Desde Notebook: empecé el curso/tutorial de la documentacion oficial de React (juego ta-te-ti)

--- HolaMundo/ReactJS/proyectos/hola-mundo/src/App.js
+++ HolaMundo/ReactJS/proyectos/hola-mundo/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Cabecera from "./components/Cabecera";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -11,19 +10,10 @@ function App() {
   return (
     <div className="App">
       <Cabecera miau={"Bienvenido miau"} manejaClick={manejaClick} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bienvenidos</p>
-        <p>Holis :3</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cabecera miau={"Bienvenido guau"} manejaClick={manejaClick} />
+      <body>
+        <p>Hola mundo</p>
+      </body>
     </div>
   );
 }
