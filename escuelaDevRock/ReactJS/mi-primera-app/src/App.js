0eecc12266b69ad05567b11f9cc08d849aad6642
MarcosDev98 <marcosolda@outlook.es>
2020-08-28 15:51:56 -0300

    Desde PC: Hice terrible quilombo, creo que rompi todo.com

--- escuelaDevRock/ReactJS/mi-primera-app/src/App.js
+++ escuelaDevRock/ReactJS/mi-primera-app/src/App.js
@@ -2,9 +2,21 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ArtistCard from "./components/artist-card";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  return <ArtistCard> </ArtistCard>;
+  return (
+    <div className="container">
+      <div className="row">
+        <ArtistCard> </ArtistCard>
+        <ArtistCard> </ArtistCard>
+        <ArtistCard> </ArtistCard>
+        <ArtistCard> </ArtistCard>
+        <ArtistCard> </ArtistCard>
+        <ArtistCard> </ArtistCard>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- escuelaDevRock/ReactJS/mi-primera-app/src/components/artist-card.js
+++ escuelaDevRock/ReactJS/mi-primera-app/src/components/artist-card.js
@@ -10,43 +10,7 @@ const styles = {
 class ArtistCard extends React.Component {
   render() {
     return (
-      <div>
-        <div className="item">
-          <img
-            src="https://img.discogs.com/C0yIaStm5NWF-u4_z3ftK_lONDs=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-573388-1379265270-2318.jpeg.jpg"
-            alt="Logo AC/DC"
-            className="pic"
-          />
-
-          <p className="titulo">Iron Maiden</p>
-        </div>
-        <div className="item">
-          <img
-            src="https://img.discogs.com/C0yIaStm5NWF-u4_z3ftK_lONDs=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-573388-1379265270-2318.jpeg.jpg"
-            alt="Logo AC/DC"
-            className="pic"
-          />
-
-          <p className="titulo">Iron Maiden</p>
-        </div>
-        <div className="item">
-          <img
-            src="https://img.discogs.com/C0yIaStm5NWF-u4_z3ftK_lONDs=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-573388-1379265270-2318.jpeg.jpg"
-            alt="Logo AC/DC"
-            className="pic"
-          />
-
-          <p className="titulo">Iron Maiden</p>
-        </div>
-        <div className="item">
-          <img
-            src="https://img.discogs.com/C0yIaStm5NWF-u4_z3ftK_lONDs=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-573388-1379265270-2318.jpeg.jpg"
-            alt="Logo AC/DC"
-            className="pic"
-          />
-
-          <p className="titulo">Iron Maiden</p>
-        </div>
+      <div className="col-6 col-3">
         <div className="item">
           <img
             src="https://img.discogs.com/C0yIaStm5NWF-u4_z3ftK_lONDs=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-573388-1379265270-2318.jpeg.jpg"
