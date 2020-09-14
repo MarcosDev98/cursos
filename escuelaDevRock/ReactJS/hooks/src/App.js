989b8039193f04c82961e46adae61ee9b71fa292
Marcos Aguado <marcosolda@outlook.es>
2020-09-14 19:10:13 -0300

    Desde Notebook: Termine curso de ReactJS de escuelaDevRock; empecé curso de holaMundo

---
+++ escuelaDevRock/ReactJS/hooks/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+//Creando mi propio HOOK
+
+function useCuenta(init, max) {
+  const [cuenta, setCuenta] = React.useState(init);
+
+  if (cuenta > max) {
+    setCuenta(0);
+  }
+
+  return [cuenta, setCuenta];
+}
+
+//Usando el HOOK useState
+function App() {
+  const [cuenta, setCuenta] = useCuenta(0, 5);
+  return (
+    <button
+      onClick={() => {
+        setCuenta(cuenta + 1);
+      }}
+    >
+      Sumar: {cuenta}
+    </button>
+  );
+}
+
+export default App;
