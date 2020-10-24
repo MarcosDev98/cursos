517da7645eccca459c974b26c3f6a23b6098ad06
Marcos Aguado <marcosolda@outlook.es>
2020-10-24 20:36:11 -0300

    Note: terminado el primer video, listo el backend

--- Fazt/mern-stack-tasks/src/routes/task.routes.js
+++ Fazt/mern-stack-tasks/src/routes/task.routes.js
@@ -9,8 +9,36 @@ router.get("/", async (req, res) => {
   res.json(tasks);
 });
 
-router.post("/", async (req, res) => {
-  console.log(req.body);
+router.get("/:id", async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  res.json(task);
 });
+
+router.post("/", async (req, res) => {
+  
+  const {title, description} = req.body;
+  const task = new Task({title, description});
+  console.log(task);
+  await task.save();
+  res.json({status: "Task saved"});
+});
+
+
+router.put("/:id", async (req,res) => {
+  const { title, description } = req.body;
+  const newTask = {title, description};
+  await Task.findByIdAndUpdate(req.params.id, newTask)
+
+  res.json({status: "Task Updated"})
+});
+
+router.delete("/:id", async (req,res) => {
+
+  await Task.findByIdAndDelete(req.params.id);
+
+  res.json({status: "Task Deleted"})
+});
+
+
 
 module.exports = router;
