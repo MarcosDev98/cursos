94eb1466de4479253bb65bbc387973b23a2982fa
Marcos Aguado <marcosolda@outlook.es>
2020-10-29 17:54:52 -0300

    Notebook: curso MERN stack youtube

--- mern/mern-youtube/backend/controllers/categoryController.js
+++ mern/mern-youtube/backend/controllers/categoryController.js
@@ -1,3 +1,60 @@
-exports.test = (req, res) => {
-    res.send("mensaje desde categoryController.js");
+const Category = require('../models/Category');
+const { errorHandler } = require('../helpers/dberrorHandler');
+
+exports.create = (req, res) => {
+    const category = new Category(req.body);
+    category.save((err, data) => {
+        if (err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        } else{
+            res.json({data});
+        }
+        
+    })
+}
+
+
+exports.list = (req, res) => {
+    Category.find().exec((err,data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        } else{
+            res.json({data})
+        }
+    })
+}
+
+
+exports.remove = (req,res) => {
+    let category = req.category
+    category.remove((err, data) => {
+        if  (err) {
+            return res.tatus(400).json({
+                error: errorHandler(err)
+            });
+        } else{
+            res.json({
+                message: "Category successfully deleted"
+            })
+
+        }
+    })
+}
+
+
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if (err || !category){
+            return res.status(400).json({
+                error: "Category was not found or does not exists"
+            })
+        } else{
+            req.category = category;
+            next()  
+        }
+    })
 }
--- mern/mern-youtube/backend/routes/category.js
+++ mern/mern-youtube/backend/routes/category.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { test } = require("../controllers/categoryController");
+const { list, create, remove, categoryById } = require("../controllers/categoryController");
 
 
-router.get('/', test);
+router.get('/categories', list);
 
+router.post('/create',create);
 
+router.delete('/:categoryId', remove);
 
+router.param('categoryId', categoryById);
 
 module.exports = router;
