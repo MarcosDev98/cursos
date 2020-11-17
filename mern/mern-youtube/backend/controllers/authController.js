4e37cc1f6e29b9dc0ee4f2b3fdb03d92fd432fe1
Marcos Aguado <marcosolda@outlook.es>
2020-11-17 08:41:12 -0300

    note, win10: curso mern arturo, signup

--- mern/mern-youtube/backend/controllers/authController.js
+++ mern/mern-youtube/backend/controllers/authController.js
@@ -1,8 +1,21 @@
-exports.test = (req, res) => {
-  console.log("Probando");
-  return res.json({ message: "Esto funciona" });
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
+
+exports.signup = (req, res) => {
+  console.log("req.body", req.body);
+  const user = new User(req.body);
+  user.save((error, user) => {
+    console.log("reached signup endpoint");
+    if (error) {
+      return res.status(400).json({
+        message: "Please check fields, there was an error.",
+      });
+    }
+    user.salt = undefined;
+    user.hashed_password = undefined;
+    res.json({
+      user,
+    });
+  });
 };
-
-//sign up
-
-//sign in
--- mern/mern-youtube/backend/routes/auth.js
+++ mern/mern-youtube/backend/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { test } = require("../controllers/authController");
+const { signup } = require("../controllers/authController");
 
-router.post("/signup", test);
+router.post("/signup", signup);
 router.post("/signin");
 
 module.exports = router;
