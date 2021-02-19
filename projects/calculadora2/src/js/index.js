ddbb253d8f5f8cb70546c76edc1cac32466f4fa6
Marcos Aguado <marcosolda@outlook.es>
2021-02-19 11:52:51 -0300

    notebook: calculadora terminada

--- projects/calculadora2/src/js/index.js
+++ projects/calculadora2/src/js/index.js
@@ -1,6 +1,10 @@
 const screen = document.getElementById('calculator-screen'),
 keys = document.getElementById('calculator-keys')
 
+let operationStatus = false,
+    number1,
+    typeOperation
+
 screen.textContent = "0"
 
 const calculator = () =>{
@@ -19,30 +23,70 @@ const calculator = () =>{
     } 
     //detectar si se pulsó una operación matemática
     if (d.math){
-      getOperation()
+      getOperation(t, d.math)
       console.log(d.math);
     } 
     //detectar si se pulsó otra operación
     if (d.operation){
-      runOperation()
-      console.log(d.operation);
+      runOperation(d.operation)
+      
     } 
   })
 }
 
 const writeScreen = (number) => {
-  screen.textContent === '0'
+  screen.textContent === '0' || operationStatus === true
     ? screen.textContent = number
-    : screen.textContent += number
-  console.log(screen.textContent);
+    : number === '.' && !screen.textContent.includes('.')
+      ? screen.textContent += number
+      : number !== '.'
+        ? screen.textContent += number
+        : null
+
+    
+    operationStatus = false
 }
 
-const getOperation = () => {
-  console.log("getOperation()");
+const getOperation = (element, operation) => {
+  operationStatus = true
+  number1 = Number(screen.textContent)
+  typeOperation = operation
+  screen.textContent = element.textContent
+  
 }
 
-const runOperation = () =>{
-  console.log("runOperation()");
+const runOperation = (operation) =>{
+  operation === 'clear'
+    ? screen.textContent = '0'
+    : getResult(number1,typeOperation)
+  operationStatus = true
 }
 
+
+const getResult = (number1, typeOperation) =>{
+  const number2 = Number(screen.textContent)
+  let result
+  
+  switch (typeOperation) {
+    case 'add':
+      result = number1 + number2
+      break;
+    case 'minus':
+      result = number1 - number2
+      break;
+    case 'multiply':
+      result = number1 * number2
+      break;
+    case 'divide':
+      result = number1 / number2
+      break;
+    default:
+      break;
+  }
+
+  result === Infinity
+    ? screen.textContent = 'Error'
+    : screen.textContent = result
+
+}
 calculator()
