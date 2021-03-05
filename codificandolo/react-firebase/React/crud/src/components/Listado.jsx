3f658e2f374645b295e2e3bbdd08c8363f001353
Marcos Aguado <marcosolda@outlook.es>
2021-03-05 09:21:27 -0300

    notebook: curso react codificandolo

--- codificandolo/react-firebase/React/crud/src/components/Listado.jsx
+++ codificandolo/react-firebase/React/crud/src/components/Listado.jsx
@@ -1,53 +1,76 @@
-import React, {Fragment, useState} from 'react';
-import uniqid from 'uniqid'
+import React, { Fragment, useState } from 'react';
+import uniqid from 'uniqid';
 
+function Listado() {
+  const [nombre, setNombre] = useState('');
+  const [listado, setListado] = useState([]);
 
-function Listado(){
+  const RegistrarNombre = (e) => {
+    e.preventDefault();
 
-  const [nombre, setNombre] = useState("")
-  const [listado, setListado] = useState([])
-
-
-
-  const RegistrarNombre = (e) =>{
-    e.preventDefault()
-    
     const nuevoNombre = {
       id: uniqid(),
       nombre: nombre,
-    }
-      
-    setListado([...listado, nuevoNombre])
+    };
+
+    setListado([...listado, nuevoNombre]);
+    setNombre('');
+  };
+
+  const borrarNombre = (id) => {
+    const nuevoArray = listado.filter( item => item.id !== id)
+    setListado(nuevoArray)
   }
 
-
-  return(
+  return (
     <Fragment>
       <h2 className="h2">APLICACIÓN CRUD BÁSICA</h2>
       <div className="row">
         <div className="col-md-8">
           <h2>Formulario para agregar nombres</h2>
-          <form onSubmit={(e) => {RegistrarNombre(e)}} className="form-group">
+          <form
+            onSubmit={(e) => {
+              RegistrarNombre(e);
+            }}
+            className="form-group"
+          >
             <input
-            id="inputNombre" 
-            className="form-control mt-3" 
-            placeholder="Introduce el Nombre" 
-            type="text"
-            onChange={(e) => {
-              setNombre(e.target.value)
-            }}/>
-            <input 
-            className="btn btn-info btn-block mt-3" 
-            value="Registrar nombre" 
-            type="submit"/>
+              id="inputNombre"
+              className="form-control mt-3"
+              placeholder="Introduce el Nombre"
+              type="text"
+              onChange={(e) => {
+                setNombre(e.target.value);
+              }}
+              value={nombre}
+            />
+            <input
+              className="btn btn-info btn-block mt-3"
+              value="Registrar nombre"
+              type="submit"
+            />
           </form>
         </div>
         <div className="col-md-4">
-        <h2>Listado de Nombres</h2>
+          <h2>Listado de Nombres</h2>
+          <ul className="list-group">
+            {listado.map( item => 
+              <li 
+              key={item.id} 
+              className="list-group-item">
+                <button 
+                  className="btn btn-danger float-right"
+                  onClick={()  => borrarNombre(item.id)}>
+                  BORRAR
+                </button>
+                {item.nombre}
+              </li>
+            )}
+          </ul>
         </div>
       </div>
     </Fragment>
-  )
+  );
 }
 
-export default Listado
+export default Listado;
