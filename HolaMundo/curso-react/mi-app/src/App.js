26c0189f3196dae4ba16d3b90a93cc9a5b635715
= <=>
2021-10-11 16:43:16 -0300

    curso Hola Mundo

--- HolaMundo/curso-react/mi-app/src/App.js
+++ HolaMundo/curso-react/mi-app/src/App.js
@@ -1,12 +1,42 @@
-import Button from './button';
+import { Component } from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hola Mundo</h1>
-      <Button onClick={() => console.log('clickeado')}>Enviar</Button>
-    </div>
-  )
+class Input extends Component {
+
+  render() {
+    return(
+      <input
+        value={this.props.value}
+        onChange={this.props.onChange}
+      />
+    )
+  }
 }
 
-export default App;
+
+class App extends Component {
+  state = {
+    nombre: '',
+    apellido: '',
+  }
+
+  updateValues = (prop, value) => {
+    this.setState({ [prop]: value })
+  }
+render() {
+  return(
+    <p>
+      Nombre completo: {`${this.state.nombre} ${this.state.apellido}`}
+      <Input
+        value={this.state.nombre}
+        onChange={(e) => this.updateValues('nombre', e.target.value)}
+        />
+      <Input
+      value={this.state.apellido}
+      onChange={(e) => this.updateValues('apellido', e.target.value)}
+      />
+    </p>
+  )
+  }
+}
+
+export default App
