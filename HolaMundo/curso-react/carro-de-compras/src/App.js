c89782afca0097f1d8bbe0d5a02d42b0f602acc0
= <=>
2021-10-12 10:46:43 -0300

    curso react HolaMundo

--- HolaMundo/curso-react/carro-de-compras/src/App.js
+++ HolaMundo/curso-react/carro-de-compras/src/App.js
@@ -1,5 +1,8 @@
 import { Component } from 'react';
 import Productos from './components/Productos';
+import Layout from './components/Layout';
+import Title from './components/Title';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   state = {
@@ -7,15 +10,40 @@ class App extends Component {
       {name: 'Tomate', price: 1500, img: '/productos/tomate.jpg'},
       {name: 'Arbejas', price: 500, img: '/productos/arbejas.jpg'},
       {name: 'Lechuga', price: 1000, img: '/productos/lechuga.jpg'},
-    ]
+    ],
+    carro: [],
   }
+
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state
+    if (carro.find(x => x.name === producto.name)) {
+      const newCarro = carro.map(x => x.name === producto.name
+        ? ({
+          ...x,
+          cantidad: x.cantidad + 1
+        })
+        : x)
+      return this.setState({ carro: newCarro })
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        <Productos
-          agregarAlCarro={() => console.log('No hace nada')}
-          productos={this.state.productos}
-        />
+        <Navbar carro={this.state.carro} />
+        <Layout>
+          <Title />
+          <Productos
+            agregarAlCarro={this.agregarAlCarro}
+            productos={this.state.productos}
+          />
+        </Layout>
       </div>
     );
   }
