76945b61b2b95a52027c85edffce7ed6857a0b28
= <=>
2021-10-27 12:20:26 -0300

    curso React HolaMundo udemy

---
+++ HolaMundo/curso-react/gestor-usuarios/src/App.js
@@ -0,0 +1,44 @@
+import useFormulario from "./hooks/useFormulario";
+import { Button, Card, Container, Input } from './components';
+
+function App() {
+  const [formulario, handleChange] = useFormulario({
+    name: '',
+    lastname: '',
+    email: ''
+  })
+
+  console.log(formulario);
+  return (
+    <Container>
+      <Card>
+        <div style={{ padding: 20 }}>
+          <form>
+            <Input
+              label="Nombre"
+              name="name"
+              value={formulario.name}
+              onChange={handleChange}
+            />
+            <Input
+              label="Apellido"
+              name="lastname"
+              value={formulario.lastname}
+              onChange={handleChange}
+            />
+            <Input
+              label="Correo"
+              name="email"
+              value={formulario.email}
+              onChange={handleChange}
+            />
+            <Button>Enviar</Button>
+          </form>
+        </div>
+      </Card>
+    </Container>
+    
+  );
+}
+
+export default App;
